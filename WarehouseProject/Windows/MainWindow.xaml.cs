fec4061644d1753a2aeb0184bd80a9b2d96822fa
Артём <artem.zeynetdinov@mail.ru>
2022-04-17 15:59:07 +0300

    Update

--- WarehouseProject/Windows/MainWindow.xaml.cs
+++ WarehouseProject/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WarehouseProject.Windows;
 
 namespace WarehouseProject
 {
@@ -33,6 +34,13 @@ namespace WarehouseProject
         private void Hyperlink_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void bntWelcome_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuWindow openwindow = new MainMenuWindow();
+            openwindow.Show();
+            this.Close();
         }
     }
 }
