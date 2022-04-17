efd76ae72f4c7dcb5aa5ef2814db446e89bf1d70
Артём <artem.zeynetdinov@mail.ru>
2022-04-17 22:00:58 +0300

    Update

---
+++ WarehouseProject/Pages/MainMenu.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarehouseProject.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Hyperlink_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnWorkingWithProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSortinTheNameList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSortingTheName_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSortingSectionNumber_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSortingArticle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReference_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGenerateReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bntExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bntDeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bntWelcome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WarehouseProject/Windows/TransitionWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WarehouseProject.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для TransitionWindow.xaml
+    /// </summary>
+    public partial class TransitionWindow : Window
+    {
+        public TransitionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Source = new Uri("..\\..\\Pages\\MainMenu.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void btnMainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Source = new Uri("..\\..\\Pages\\HomePage.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Source = new Uri("..\\..\\Pages\\AddProductPage.xaml", UriKind.RelativeOrAbsolute);
+        }
+    }
+}
